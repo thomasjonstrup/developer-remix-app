eb8a1b21eb6b4c47121923a632ad05c1b78b95c8
Thomas Jonstrup <thomas_jonstrup@hotmail.com>
2021-11-25 22:21:24 +0100

    Followed blog tutorial

--- remix.config.js
+++ remix.config.js
@@ -1,11 +1,20 @@
 /**
  * @type {import('@remix-run/dev/config').AppConfig}
  */
+/* module.exports = {
+	appDirectory: 'app',
+	browserBuildDirectory: 'public/build',
+	publicPath: '/build/',
+	serverBuildDirectory: 'build',
+	//serverBuildDirectory: 'netlify/functions/server/build',
+	devServerPort: 8888,
+};
+ */
+
 module.exports = {
 	appDirectory: 'app',
 	browserBuildDirectory: 'public/build',
 	publicPath: '/build/',
-	//serverBuildDirectory: 'build',
-	serverBuildDirectory: 'netlify/functions/server/build',
-	devServerPort: 8888,
+	serverBuildDirectory: 'build',
+	devServerPort: 8002,
 };
